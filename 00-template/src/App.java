a34245c6daf7b11a351a3608a923e2db9914bd68
Meyesss <146051966+Meyesss@users.noreply.github.com>
2023-10-31 13:20:05 +0800

    kuliah algo 31/10/23

---
+++ 00-template/src/App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Program Biodata Mahasiswa
+        // Deklarasi variable
+        String nama;
+        String nim;
+        String tgl_lahir;
+        String alamat;
+        Float berat_badan;
+        Float tinggi_badan;
+        
+        // mengisi variable
+        nama = "Ika";
+        nim = "23241059";
+        tgl_lahir = "10 0kt0ber 2004";
+        alamat = "bonder";
+        berat_badan = 53f;
+        tinggi_badan = 140f;
+
+        //cetak biodata
+        System.out.println("==============")
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+        System.out.println("Tanggal Lahir :" + tgl_lahir);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Berat Badan : " + berat_badan);
+        System.out.println("Tinggi Badan : " + tinggi_badan);
+        System.out.println("===============");
+ 
+
+
+        
+
