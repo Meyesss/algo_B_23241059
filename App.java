8e99f20e972d54272720174dcfd1c9c5089492a0
Meyesss <146051966+Meyesss@users.noreply.github.com>
2023-12-28 12:20:48 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,22 @@
+java.util.Scanner;
+public class App{
+    Public Static void main(String[] args) throws Exception
+
+    // DATA MEMBER
+    int Total_Belanja = 0;
+
+    // membuat scanner menerima input keyboard
+    Scanner Scan = new Scanner (System.in);
+}
+    // mengambil input dari keyboard 
+    System.out.print(s: "Masukkan Total Belanja");
+    Total belanja = Scan.next.in   ();
+
+    // cek total belanja 
+    if (Total   belanja >=  5000){
+        System.out.println(x:"Belanja Anda Kurang dari Rp. 50000");
+
+}
+       else {
+        System.out.println(x:"Terima Kasih Belanja di Toko Kami");
+       }
