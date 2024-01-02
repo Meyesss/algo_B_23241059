ae9272812cda784ecf4bfac88e694451b6596dec
Meyesss <146051966+Meyesss@users.noreply.github.com>
2024-01-02 15:18:02 +0700

    Add files via upload

---
+++ ja1/src/honorlembur.java
@@ -0,0 +1,32 @@
+import Java.Util.Scanner;
+
+public class main {
+    public static main (string[] args) {
+        Scanner input = New scanner(System.in);
+
+        System.out.println("karyawan tetap")
+        int Jam lembur = Input next.in();
+
+        System.out.println("12 jam");
+        Int Tarif perjam = Input Next.in();
+        
+        Int Total honor = Hitung Honor lembur("karyawan tetap, 12 jam");
+
+        System.out.println(Total Honor Lembur adalah: "Rp.200.000");
+     }
+        Public Static int Hitung Honor Lembur(karyawan tetap int 12 jam) {
+            if ("karyawan tetap")
+                if (Jam lembur >= 12 jam) {
+                      return = 200.000
+                } else (Jam lembur >= 8) {
+                    return = 100.000
+                }
+                } else {
+                    if (Jam Lembur >= 8) {
+                        return = 75.000
+                }
+                    return 0;
+
+    }
+}
+    
